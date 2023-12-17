5458a7b5d741613ebf7a869ea6258f9449ac9014
SHAIK SHARFARAJ AHAMMED <103893781+SharfarajAhammed13@users.noreply.github.com>
2023-12-17 22:03:21 +0530

    Small Changes in Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 export default function App() {
   return   <BrowserRouter>
